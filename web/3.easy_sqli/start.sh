63e11b8b1c9583bb282b515d4ce9ee4fea616936
1683793776@qq.com <1683793776@qq.com>
2023-05-02 21:16:14 +0800

    add web3: easy_sqli

---
+++ web/3.easy_sqli/start.sh
@@ -0,0 +1,17 @@
+rm  /start.sh
+echo $GZCTF_FLAG > /flag
+export GZCTF_FLAG="no_flag"
+service mysql start
+until mysql  -u "root" "-proot" -e 'SELECT 1'; do
+  >&2 echo "MySQL is unavailable - sleeping"
+  sleep 1
+done
+>&2 echo "MySQL is up - executing command"
+mysql -u root -e "CREATE DATABASE ctf"
+mysql -u root -proot ctf < /db.sql
+mysql -e "CREATE USER 'ctf'@'%' IDENTIFIED BY 'ctf';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'ctf'@'%';"
+rm /db.sql
+rm /flag
+service apache2 start &
+tail -f /dev/null
